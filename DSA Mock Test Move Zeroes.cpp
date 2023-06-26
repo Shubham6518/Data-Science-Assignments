76c8fecae331890d48f5c8007f1878226bcb6bd1
Shubham6518 <123873333+Shubham6518@users.noreply.github.com>
2023-06-26 13:27:14 +0530

    Create DSA Mock Test Move Zeroes.cpp

---
+++ DSA Mock Test Move Zeroes.cpp
@@ -0,0 +1,28 @@
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)cin>>arr[i];
+        
+        cout<<"Input Array:- ";
+        for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+        int i=0;
+        while(i<n){
+            if(arr[i]==0){
+                int j=i+1;
+                while(j<n){
+                    if(arr[j]!=0) {swap(arr[i], arr[j]); break;} 
+                    j++;
+                }
+            } i++;
+        }
+        cout<<endl<<"Modified array:- ";
+        for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
